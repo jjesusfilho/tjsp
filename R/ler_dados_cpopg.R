a65ce96e5e5369a70ac310f6ddd9cebd90040a79
jjesusfilhogit <jjesusfilho@gmail.com>
2018-11-29 00:44:41 -0200

    novas correções

--- R/ler_dados_cpopg.R
+++ R/ler_dados_cpopg.R
@@ -10,34 +10,30 @@
 #' ler_dados_cpopg()
 #' }
 #'
-ler_dados_cpopg<-function(diretorio="."){
+ler_dados_cpopg<-function (diretorio = ".")
+{
+  arquivos <- list.files(path = diretorio, pattern = ".html",
+                         full.names = TRUE)
 
-  arquivos<-list.files(path = diretorio,pattern=".html",full.names=TRUE)
-
-  processos<-stringr::str_extract(arquivos,"\\d{20}") %>%
-    as.numeric()
-
+  processos <- stringr::str_extract(arquivos, "\\d{20}")
 
   future::plan("multiprocess")
 
-  furrr::future_map2_dfr(arquivos,processos,purrr::possibly(~{
-
+  furrr::future_map2_dfr(arquivos, processos, purrr::possibly(~{
     .x %>%
       xml2::read_html() %>%
-      rvest::html_nodes(xpath="//table[@class='secaoFormBody']") %>%
-      rvest::html_table(fill=T) %>%
+      rvest::html_nodes(xpath = "//table[@class='secaoFormBody']") %>%
+      rvest::html_table(fill = T) %>%
       purrr::pluck(2) %>%
-      dplyr::filter(stringr::str_detect(X1,":")) %>%
-      tidyr::unite(coluna,c("X1","X2"),sep="") %>%
+      dplyr::filter(stringr::str_detect(X1, "\\w{1,20}:")) %>%
+      tidyr::unite(coluna, c("X1", "X2"), sep = "") %>%
       dplyr::select(coluna) %>%
-      tidyr::separate(coluna,c("variavel","valor"),sep=":") %>%
+      tidyr::separate(coluna, c("variavel", "valor"), sep = ":") %>%
       dplyr::mutate_all(dplyr::funs(stringr::str_trim(.) %>%
-                               stringr::str_squish())) %>%
-      dplyr::mutate(processo=rlang::UQ(rlang::quo(.y)),
-                    valor=stringr::str_remove(valor,"NA$")) %>%
-      dplyr::mutate_if(is.character,as.factor) %>%
-      dplyr::select(processo,variavel,valor)
+                                      stringr::str_squish())) %>%
+      dplyr::mutate(processo = rlang::UQ(rlang::quo(.y)), valor = stringr::str_remove(valor, "NA$")) %>%
+      dplyr::mutate_if(is.character, as.factor) %>%
+      dplyr::select(processo, variavel,valor)
 
-  },NULL),.progress = TRUE)
-
+  }, NULL), .progress = TRUE)
 }
