bc5376701b6c3ff508d79acd8ffead6f04046456
jjesusfilho <jjesusfilho@gmail.com>
2019-02-05 19:54:13 -0200

    tudo

---
+++ R/baixar_serializar.R
@@ -0,0 +1,33 @@
+#' Aplica uma função para baixar htmls e serializa os htmls em grupos de
+#'     no máximo aproximadamente 300 Mb.
+#'
+#' @param lista lista de vetores de processos ou simplesmente um vetor de processos
+#' @param dir  diretorio onde serão criados os subdiretorios
+#' @param subdir subdiretórios onde serão armazenados  grupo da lista.
+#' @param funcao Função de download do html, ex. esaj::download_cpopg
+#'
+#' @return Arquivo em rds baixado
+#' @export
+#'
+baixar_serializar <- function(lista, dir=".", subdir=NULL,funcao = NULL) {
+
+  diretorios <- fs::dir_create(paste0(dir,"/",subdir))
+
+  purrr::walk2(lista, diretorios,  ~ {
+    funcao(.x, .y)
+
+    fs::file_info(fs::dir_ls(.y, glob = "*.html")) %>%
+      .[c(1, 3)] %>%
+      dplyr::filter(size <= fs::as_fs_bytes("86K")) %>%
+      dplyr::pull("path") %>%
+      fs::file_delete()
+
+    serializar_tjsp(origem = .y,
+                    destino = .y,
+                    prefix = "arquivo")
+    fs::dir_ls(.y, glob = "*.html") %>%
+      fs::file_delete()
+
+  })
+
+}
---
+++ R/codigos.R
@@ -0,0 +1,12 @@
+#' @title Códigos
+#' @description Códigos dos distribuidores do Tribunal de Justiça de São Paulo
+#' @format A data frame with 388 rows and 5 variables:
+#' \describe{
+#'   \item{\code{segmento}}{character segmento do Judiciário conforme resolução 65 CNJ}
+#'   \item{\code{uf}}{double Código do estado conforme resolução 65 do CNJ}
+#'   \item{\code{comarca}}{character Nome da comarca}
+#'   \item{\code{distribuidor}}{double Código do distribuidor conforme resolução 65 CNJ}
+#'   \item{\code{descricao}}{character Descrição do distribuidor}
+#'}
+#' @source \url{https://tjsp.jus.br/}
+"codigos"
