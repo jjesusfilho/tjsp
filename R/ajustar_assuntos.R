903984b71b0c4b7dfc5552b1aac58b1b934d5bf9
jjesusfilho <jjesusfilho@gmail.com>
2019-04-18 16:31:16 -0300

    novos ajustes na função ajustar_assuntos

--- R/ajustar_assuntos.R
+++ R/ajustar_assuntos.R
@@ -10,13 +10,16 @@ ajustar_assuntos <- function(df){
   df %>%
     dplyr::select(assunto) %>%
     dplyr::distinct() %>%
-    dplyr::mutate(assunto_=stringr::str_remove(assunto,"^\\d.+?\\- ") %>%
+    dplyr::mutate(assunto_= stringr::str_remove(assunto,"^\\d.+?\\- ") %>%
                     abjutils::rm_accent() %>%
                     stringr::str_to_lower() %>%
                     stringr::str_squish()
     ) %>%
-    dplyr::left_join(assuntos,by="assunto_") %>%
-    dplyr::select(assunto = assunto.x,area2=area) %>%
-    dplyr::right_join(df,by = "assunto")
+    dplyr::left_join(tjsp::assuntos,by="assunto_") %>%
+    dplyr::select(assunto= assunto.x,area2=area) %>%
+    dplyr::right_join(df,by="assunto") %>%
+    dplyr::select(processo,assunto,area,area2,classe,juiz,vara,foro,digital,dplyr::everything())
+
 
 }
+
