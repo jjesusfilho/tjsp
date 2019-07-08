3a6adc802b820e2b9e23ee59084114ae2359e5fd
José de Jesus Filho <jjesusfilho@gmail.com>
2019-07-08 18:39:57 -0300

    pequenos ajustes.

--- R/filtrar_cjsg.R
+++ R/filtrar_cjsg.R
@@ -1,9 +1,9 @@
 #' Filtra base cjsg por assunto e classe.
 #'
-#' @param base base parseada os cjsg htmls
+#' @param base base parseada dos cjsg htmls
 #' @param assunto regex do assunto a ser filtrado
 #' @param classe regex da classe a ser filtrada
-#' @param negate_assunto coleque TRUE se quiser excluir os assuntos especificados.
+#' @param negate_assunto coloque TRUE se quiser excluir os assuntos especificados.
 #'
 #' @details Geralmente as classes são limitadas e as descrições mais claras que
 #'     os assuntos. Por isso optei pela negação somente do assunto.
