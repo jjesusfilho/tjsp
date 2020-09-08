28e2a2464e8bf049ed44c1113f52d7d18a15375a
José de Jesus Filho <jjesusfilho@gmail.com>
2020-09-07 21:32:51 -0300

    adiciona novas funções

---
+++ R/tjsp_baixar_tabela_docs.R
@@ -0,0 +1,88 @@
+#' Baixa tabelas dos documentos do processo
+#'
+#' @param processos Número do processo
+#' @param diretorio Diretório
+#'
+#' @return html
+#' @export
+#'
+tjsp_baixar_tabela_docs <- function(processos, diretorio = "."){
+
+
+
+  httr::set_config(httr::config(ssl_verifypeer = FALSE))
+
+  processos <- stringr::str_remove_all(processos, "\\D+") %>%
+    stringr::str_pad(width = 20, "left", "0") %>%
+    abjutils::build_id()
+
+  uri1 <- "https://esaj.tjsp.jus.br/cposg/search.do?"
+
+  pb <- progress::progress_bar$new(total = length(processos))
+
+  purrr::walk(processos, purrr::possibly(~{
+
+    pb$tick()
+
+    r<-  httr::GET("https://esaj.tjsp.jus.br/cposg/open.do?gateway=true")
+
+    p <- .x
+
+    unificado <- p %>%
+      stringr::str_extract(".{15}")
+
+    foro <- p %>%
+      stringr::str_extract("\\d{4}$")
+
+    query1<-  list(conversationId = "", paginaConsulta = "1", localPesquisa.cdLocal = "-1",
+                   cbPesquisa = "NUMPROC", tipoNuProcesso = "UNIFICADO", numeroDigitoAnoUnificado = unificado,
+                   foroNumeroUnificado = foro, dePesquisaNuUnificado = p,
+                   dePesquisa = "", uuidCaptcha = "", pbEnviar = "Pesquisar")
+
+    resposta1 <- httr::RETRY("GET",
+                             url = uri1, query = query1,
+                             quiet = TRUE, httr::timeout(2)
+    )
+
+    conteudo1 <- httr::content(resposta1)
+
+    if (xml2::xml_find_first(conteudo1, "boolean(//div[@id='listagemDeProcessos'])")) {
+      conteudo1 <- xml2::xml_find_all(conteudo1, "//a[@class='linkProcesso']") %>%
+        xml2::xml_attr("href") %>%
+        xml2::url_absolute("https://esaj.tjsp.jus.br") %>%
+        purrr::map(~ httr::RETRY("GET", .x, httr::timeout(2)) %>%
+                     httr::content())
+    } else {
+      conteudo1 <- list(conteudo1)
+    }
+
+    purrr::walk(conteudo1,purrr::possibly(~{
+
+
+
+      cdProcesso <- .x %>%
+        xml2::xml_find_all("//input[@name='cdProcesso']") %>%
+        xml2::xml_attr("value")
+
+
+      arquivo <- file.path(diretorio,paste0("tabela_cposg_docs_processo_",stringr::str_remove_all(p,"\\D"),".html"))
+
+      url1  <- paste0("https://esaj.tjsp.jus.br/cposg/verificarAcessoPastaDigital.do?cdProcesso=",cdProcesso,"&conversationId=&_=1599440192646")
+
+      url2 <- httr::GET(url1) %>%
+        httr::content("text")
+
+
+
+      httr::GET(url2,httr::write_disk(arquivo,overwrite=TRUE))
+
+
+    },NULL))
+  },NULL))
+
+  }
+
+
+
+
+
---
+++ R/tjsp_ler_tabela_docs.R
@@ -0,0 +1,48 @@
+#' Ler tabelas baixadas com tjsp_baixar_tabela_docs
+#'
+#' @param arquivos Vetor de arquivos
+#' @param diretorio Informar se não informar diretório
+#'
+#' @return tibble
+#' @export
+#'
+tjsp_ler_tabela_docs <- function(arquivos = NULL, diretorio = "."){
+
+  if (is.null(arquivos)) {
+
+    arquivos <- list.files(diretorio,full.names = TRUE)
+
+  }
+
+  pb <- progress::progress_bar$new(total = length(arquivos))
+
+  purrr::map_dfr(arquivos,purrr::possibly(~{
+
+    pb$tick()
+
+    processo <- stringr::str_extract(.x,"\\d{20}")
+
+    .x %>%
+      xml2::read_html() %>%
+      xml2::xml_text() %>%
+      stringr::str_extract("(?<=requestScope = )\\X+?(?=;)") %>%
+      jsonlite::fromJSON() %>%
+      tidyr::unnest_longer(children,names_repair="unique") %>%
+      purrr::flatten_dfc() %>%
+      dplyr::select(dplyr::matches("(title|parametros)")) %>%
+      setNames(c("doc_name","paginas","url_doc")) %>%
+      tibble::rownames_to_column("doc_id") %>%
+      dplyr::mutate(pagina_inicial = stringr::str_extract(paginas,"\\d+"),
+                    pagina_final = stringr::str_extract(paginas,"\\d+$"),
+                    processo = processo,
+                    url_doc = paste0("https://esaj.tjsp.jus.br/pastadigital/getPDF.do?",url_doc)) %>%
+      dplyr::select(processo, doc_id,doc_name,paginas,pagina_inicial, pagina_final, url_doc)
+
+
+
+
+  },NULL))
+
+
+
+}
