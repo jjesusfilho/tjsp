3fa313d7ea98d6bbf75726e6d8c2afa227f33b53
José de Jesus Filho <jjesusfilho@gmail.com>
2020-12-15 19:27:27 -0300

    ajuste tjsp_cpopg_baixar_tabela_docs

--- R/tjsp_cpopg_baixar_tabela_docs.R
+++ R/tjsp_cpopg_baixar_tabela_docs.R
@@ -11,15 +11,15 @@ tjsp_cpopg_baixar_tabela_docs <- function (processos = NULL, diretorio = ".")
   httr::set_config(httr::config(ssl_verifypeer = FALSE))
   processos <- stringr::str_remove_all(processos, "\\D+") %>%
     stringr::str_pad(width = 20, "left", "0") %>% abjutils::build_id()
-  
+
   uri1 <- "https://esaj.tjsp.jus.br/cpopg/search.do?gateway=true"
-  
+
   pb <- progress::progress_bar$new(total = length(processos))
-  
+
   purrr::map_dfr(processos, purrr::possibly(~{
-    
+
     pb$tick()
-    
+
     p <- .x
     unificado <- p %>% stringr::str_extract(".{15}")
     foro <- p %>% stringr::str_extract("\\d{4}$")
@@ -31,30 +31,29 @@ tjsp_cpopg_baixar_tabela_docs <- function (processos = NULL, diretorio = ".")
     resposta1 <- httr::RETRY("GET", url = uri1, query = query1,
                              quiet = TRUE, httr::timeout(2))
     conteudo1 <- httr::content(resposta1)
-    
+
     if (xml2::xml_find_first(conteudo1, "boolean(//div[@id='listagemDeProcessos'])")) {
       conteudo1 <- xml2::xml_find_all(conteudo1, "//a[@class='linkProcesso']") %>%
         xml2::xml_attr("href") %>% xml2::url_absolute("https://esaj.tjsp.jus.br") %>%
         purrr::map(~httr::RETRY("GET", .x, httr::timeout(2)) %>%
                      httr::content())
-    }
-    else
+    } else {
       conteudo1 <- list(conteudo1)
-    
+    }
     purrr::walk(conteudo1,purrr::possibly(~{
-      
+
       arquivo <- file.path(diretorio,paste0("tabela_cpopg_docs_processo_",stringr::str_remove_all(p,"\\D"),".html"))
-      
-      
+
+
       url1 <- .x %>%
-        xml2::xml_find_first("//a[@class='linkPasta']") %>%
-        xml2::xml_attr("href") %>% 
+        xml2::xml_find_first("//a[@id='linkPasta']") %>%
+        xml2::xml_attr("href") %>%
         paste0("https://esaj.tjsp.jus.br",.)
-      
+
 
       httr::GET(url1,httr::write_disk(arquivo,overwrite = TRUE))
-        
-      
+
+
     },NULL))
   }, NULL))
 }
