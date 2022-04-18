24d678da3b7b3133df9f8613b67b3d22277e56ea
José de Jesus Filho <jjesusfilho@gmail.com>
2022-04-18 08:45:33 -0300

    ajusta função lapso

--- R/lapso.R
+++ R/lapso.R
@@ -1,32 +1,33 @@
 #' Retorna o lapso temporal na unidade especificada entre duas datas.
 #'
-#' @param data_inicial incluir a data no formato Date.
-#' @param data_final  incluir a data no formato Date.
-#' @param unidade  uma das opções: ano, mes, semana, dia.
+#' @param data_inicial data inicial
+#' @param data_final data final
+#' @param unidade ano, mes, semana ou dia.
 #' @details Esta função é um mero wrapper das funções interval e time_length
-#'     do pacote lubridate. Ela considera as variações de ano, meses e dias.
+#'     do pacote lubridate. Ela considera as variações de
+#'     ano, mes,semana, dia, hora, minuto e segundo.
 #'
 #' @return double com o lapso temporal
 #' @export
 #'
 #' @examples
-#' lapso(data_inicial = as.Date("2018-01-01"), data_final = as.Date("2018-12-31"), unidade = "mes")
-lapso <- function(data_inicial = NULL,
-                  data_final = NULL,
-                  unidade = "mes") {
-  if (!lubridate::is.Date(data_inicial) |
-    !lubridate::is.Date(data_final)) {
-    stop("As datas devem estar no formato iso 8601: yyyy-mm-dd")
+#' lapso(data_inicial=as.Date("2018-01-01"),data_final=as.Date("2018-12-31"),unidade="mes")
+
+
+lapso <- function (data_inicial = NULL, data_final = NULL, unidade = "mes")
+{
+  if (!lubridate::is.timepoint(data_inicial) | !lubridate::is.timepoint(data_final)) {
+    stop("As datas devem no formato data")
   }
+  periodo <- switch(unidade,
+                    ano = "year",
+                    mes = "month",
+                    semana = "week",
+                    dia = "day",
+                    hora = "hour",
+                    minuto = "minute",
+                    segundo = "second")
 
-  periodo <- switch(
-    unidade,
-    "ano" = "year",
-    "mes" = "month",
-    "semana" = "week",
-    "dia" = "day"
-  )
-
-  lubridate::interval(data_inicial, data_final) %>%
+  lubridate::interval(data_inicial, data_final) |>
     lubridate::time_length(periodo)
 }
