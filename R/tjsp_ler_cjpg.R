0ce9bf282213a7f5d407243d81602d6b37c4afdb
José de Jesus Filho <jjesusfilho@gmail.com>
2022-05-28 22:30:28 -0300

    remanufaturação da função tjsp_ler_cjpg

--- R/tjsp_ler_cjpg.R
+++ R/tjsp_ler_cjpg.R
@@ -11,80 +11,101 @@
 #' \dontrun{
 #' tjsp_ler_cjpg()
 #' }
-tjsp_ler_cjpg<-function (arquivos = NULL, diretorio = ".")
+tjsp_ler_cjpg <- function(arquivos = NULL, diretorio = ".")
 {
-
-  if (is.null(arquivos)){
-
+  if (is.null(arquivos)) {
     arquivos <- list.files(path = diretorio, pattern = ".html",
                            full.names = TRUE)
   }
 
   pb <- progress::progress_bar$new(total = length(arquivos))
 
-  df <- purrr::map_dfr(arquivos, purrr::possibly(~{
+  purrr::map_dfr(arquivos, purrr::possibly(~{
 
     pb$tick()
-
-    x <- .x %>% xml2::read_html(encoding = "UTF-8")
-
-    cd_doc <- x %>%
-      xml2::xml_find_all("//a[@title='Visualizar Inteiro Teor']") %>%
-      xml2::xml_attr("name")
-
-    resposta <- x %>%
-      xml2::xml_find_all(xpath = "//*[@id='divDadosResultado']/table//td//td[@align='left']") %>%
-      xml2::xml_text(trim = TRUE) %>% stringr::str_squish() %>%
-      paste0(collapse = "\n") %>% stringi::stri_split_regex("\n(?=\\d{4,})") %>%
-      unlist()
 
     pagina <- stringr::str_extract(.x, "(?<=pagina_)\\d+") %>%
       as.integer()
 
-    hora_coleta <- stringr::str_extract(.x, "\\d{4}[\\d_/]+") %>%
-      lubridate::ymd_hms(tz="America/Sao_Paulo")
+    suppressWarnings(
+      hora_coleta <- stringr::str_extract(.x, "\\d{4}[\\d_/]+") %>%
+        lubridate::ymd_hms(tz = "America/Sao_Paulo")
 
-    processo <- stringi::stri_extract_first_regex(resposta,
-                                                  "\\d+-\\d{2}\\.\\d{4}\\.\\d\\.\\d{2}\\.\\d{4}")
+    )
 
-    classe <- stringi::stri_extract_first_regex(resposta,
-                                                "Classe:.*")
-
-    assunto <- stringi::stri_extract_first_regex(resposta,
-                                                 "Assunto:.*")
-
-    magistrado <- stringi::stri_extract_first_regex(resposta,
-                                                    "Magistrado:.*")
-
-    comarca <- stringi::stri_extract_first_regex(resposta,
-                                                 "Comarca:.*")
-
-    foro <- stringi::stri_extract_first_regex(resposta, "Foro:.*")
+    x <- .x |>  xml2::read_html(encoding = "UTF-8")
 
 
-    vara <- stringi::stri_extract_first_regex(resposta, "Vara:.*")
+    processo <- x %>%
+      xml2::xml_find_all(xpath = "//tr[@class='fundocinza1']") |>
+      purrr::map(~xml2::xml_child(.x, "/a/span[@class='fonteNegrito']") |>
+                   xml2::xml_text(trim = TRUE)) |>
+      unlist()
 
-    disponibilizacao <- stringi::stri_extract_first_regex(resposta,
-                                                          "Data\\s+de\\s+Disponibiliza\u00e7\u00e3o:.*")
+    cd_doc <- x %>%
+      xml2::xml_find_all(xpath = "//tr[@class='fundocinza1']") |>
+      purrr::map(~xml2::xml_child(.x, "/a[@title='Visualizar Inteiro Teor']") |>
+                   xml2::xml_attr("name")) |>
+      unlist()
 
-    julgado <- x %>%
-      xml2::xml_find_all("//div[@align='justify'][@style='display: none;']") %>%
-      xml2::xml_text()
+    assunto <- x %>%
+      xml2::xml_find_all(xpath = "//tr[@class='fundocinza1']") |>
+      purrr::map(~xml2::xml_child(.x, "/tr/td//strong[contains(.,'Assunto:')]/following-sibling::text()") |>
+                   xml2::xml_text(trim = TRUE)) |>
+      unlist()
+
+    classe <- x %>%
+      xml2::xml_find_all(xpath = "//tr[@class='fundocinza1']") |>
+      purrr::map(~xml2::xml_child(.x, "/tr/td//strong[contains(.,'Classe:')]/following-sibling::text()") |>
+                   xml2::xml_text(trim = TRUE)) |>
+      unlist()
+
+    comarca <- x %>%
+      xml2::xml_find_all(xpath = "//tr[@class='fundocinza1']") |>
+      purrr::map(~xml2::xml_child(.x, "/tr/td//strong[contains(.,'Comarca:')]/following-sibling::text()") |>
+                   xml2::xml_text(trim = TRUE)) |>
+      unlist()
+
+    foro <- x %>%
+      xml2::xml_find_all(xpath = "//tr[@class='fundocinza1']") |>
+      purrr::map(~xml2::xml_child(.x, "/tr/td//strong[contains(.,'Foro:')]/following-sibling::text()") |>
+                   xml2::xml_text(trim = TRUE)) |>
+      unlist()
+
+    vara <- x %>%
+      xml2::xml_find_all(xpath = "//tr[@class='fundocinza1']") |>
+      purrr::map(~xml2::xml_child(.x, "/tr/td//strong[contains(.,'Vara:')]/following-sibling::text()") |>
+                   xml2::xml_text(trim = TRUE)) |>
+      unlist()
+
+    magistrado <- x %>%
+      xml2::xml_find_all(xpath = "//tr[@class='fundocinza1']") |>
+      purrr::map(~xml2::xml_child(.x, "/tr/td//strong[contains(.,'Magistrado:')]/following-sibling::text()") |>
+                   xml2::xml_text(trim = TRUE)) |>
+      unlist()
 
 
-    tibble::tibble(processo, pagina, hora_coleta,classe, assunto, magistrado,
-                   comarca, foro, vara, disponibilizacao, julgado, cd_doc)
+    disponibilizacao <- x %>%
+      xml2::xml_find_all(xpath = "//tr[@class='fundocinza1']") |>
+      purrr::map(~xml2::xml_child(.x, "/tr/td//strong[contains(.,'Data de Disponibilização:')]/following-sibling::text()") |>
+                   xml2::xml_text(trim = TRUE)) |>
+      unlist()
+
+    julgado <- x %>% xml2::xml_find_all("//div[@align='justify'][@style='display: none;']") %>%
+      xml2::xml_text(trim = TRUE)
+
+    tibble::tibble(processo, pagina, hora_coleta, classe,
+                   assunto, magistrado, comarca, foro, vara, disponibilizacao,
+                   julgado, cd_doc)
+
   }, NULL)) %>%
-
-    dplyr::mutate(dplyr::across(4:10, ~stringi::stri_replace_first_regex(., ".*:\\s?",""))) %>%
-
-    dplyr::mutate_if(is.character, stringr::str_squish) %>%
-
+    dplyr::mutate(dplyr::across(4:10, ~stringi::stri_replace_first_regex(.,".*:\\s?", ""))) %>% dplyr::mutate_if(is.character, stringr::str_squish) %>%
     dplyr::mutate(disponibilizacao = lubridate::dmy(disponibilizacao),
                   processo = stringr::str_remove_all(processo, "\\D+")) %>%
-
-    dplyr::mutate(duplicado = vctrs::vec_duplicate_detect(processo), .after = hora_coleta)
+    dplyr::mutate(duplicado = vctrs::vec_duplicate_detect(processo),
+                  .after = hora_coleta)
 }
+
 
 #' @rdname tjsp_ler_cjpg
 #' @export
