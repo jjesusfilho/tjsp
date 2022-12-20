11f7b4055b91d9db2468a1e94c8ca09a8b2edb81
José de Jesus Filho <jjesusfilho@gmail.com>
2022-12-20 07:28:21 -0300

    ajustes na coleta de peças

--- R/tjsp_baixar_cpopg_docs.R
+++ R/tjsp_baixar_cpopg_docs.R
@@ -70,10 +70,52 @@ tjsp_baixar_cpopg_docs <- function(df,
       r2<-  httr::GET(url1)
 
 
-      tjsp_baixar_docs(dd$processo, dd$doc_id, dd$url_doc, diretorio)
+      tjsp_baixar_docs(dd$processo, dd$doc_id, dd$pagina_inicial, dd$pagina_final,  dd$url_doc, diretorio)
 
 
     },NULL))
 
   },NULL))
+}
+
+
+
+#'
+#' @param processos Número do processo
+#' @param doc_id Id do documento
+#' @param pagina_inicial Pagina inicial
+#' @param pagina_final Pagina final
+#' @param urls Urls dos documentos
+#' @param diretorio Diretório
+#'
+#' @return pdfs
+#'
+tjsp_baixar_docs <- function(processos  = NULL,
+                             doc_id = NULL,
+                             pagina_inicial = NULL,
+                             pagina_final = NULL,
+                             urls = NULL,
+                             diretorio = NULL){
+
+
+  processos <- stringr::str_remove_all(processos,"\\D")
+
+  id <- stringr::str_c(processos,
+                       "_doc_id_",doc_id,
+                       "_pagina_inicial_", pagina_inicial,
+                       "_pagina_final_", pagina_final)
+
+  pb <- progress::progress_bar$new(total = length(processos))
+
+
+  purrr::walk2(urls,id,purrr::possibly(~{
+
+    pb$tick()
+
+    arquivo <- file.path(diretorio,paste0(.y,".pdf"))
+
+    httr::GET(.x,httr::write_disk(arquivo,overwrite = TRUE))
+
+  },NULL))
+
 }
--- R/tjsp_combinar_docs.R
+++ R/tjsp_combinar_docs.R
@@ -25,12 +25,11 @@ tjsp_combinar_docs <- function(arquivos = NULL, dir_origem = ".",  dir_destino =
 
 
 
-  lista <- tibble::tibble(arquivos) %>%
+  lista <- tibble::tibble(arquivos) |>
     dplyr::mutate(processo = stringr::str_extract(arquivos,"\\d{20}"),
-                  doc_id = stringr::str_extract(arquivos,'\\d+(?=\\.pdf)')) %>%
-    #inner_join(select(df,processo,doc_id,url_doc), by = c("processo","doc_id")) %>%
-    dplyr::mutate(doc_id = as.integer(doc_id)) %>%
-    dplyr::arrange(processo,doc_id) %>%
+                  doc_id = stringr::str_extract(arquivos,'(?<=doc_id_)\\d+') |> as.integer(),
+		  pagina_inicial = stringr::str_extract(arquivos, '(?<=inicial_)\\d+') |> as.integer()) |>
+    dplyr::arrange(processo,doc_id,pagina_inicial) |>
     dplyr::group_split(processo)
 
   purrr::walk(lista, purrr::possibly(~{
