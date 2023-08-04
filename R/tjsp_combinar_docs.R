9c01dffab3c0244acffdf6d76c77528d50c8d419
José <jjesusfilho@gmail.com>
2023-08-04 19:29:22 -0300

    Atualiza funções

--- R/tjsp_combinar_docs.R
+++ R/tjsp_combinar_docs.R
@@ -4,11 +4,15 @@
 #' @param dir_origem Diretório de origem, se não
 #'     informar arquivos
 #' @param dir_destino Diretório destino
+#' @param nivel Juntar processo inteiro ou por documento?
 #'
 #' @return único pdf
 #' @export
 #'
-tjsp_combinar_docs <- function(arquivos = NULL, dir_origem = ".",  dir_destino = NULL){
+tjsp_combinar_docs <- function(arquivos = NULL,
+                               dir_origem = ".",
+                               dir_destino = NULL,
+                               nivel = c("processo", "doc")){
 
   if (is.null(dir_destino) || !dir.exists(dir_destino)){
 
@@ -22,23 +26,44 @@ tjsp_combinar_docs <- function(arquivos = NULL, dir_origem = ".",  dir_destino =
 
   }
 
-
+  nivel = nivel[1]
 
 
   lista <- tibble::tibble(arquivos) |>
     dplyr::mutate(processo = stringr::str_extract(arquivos,"\\d{20}"),
                   id_doc = stringr::str_extract(arquivos,'(?<=id_doc_)\\d+') |> as.integer(),
 		  pagina_inicial = stringr::str_extract(arquivos, '(?<=inicial_)\\d+') |> as.integer()) |> # nolint
-    dplyr::arrange(processo,id_doc,pagina_inicial) |>
-    dplyr::group_split(processo)
+    dplyr::arrange(processo,id_doc,pagina_inicial)
 
-  purrr::walk(lista, purrr::possibly(~{
+  if (nivel == "processo"){
 
-    processo <- unique(.x$processo)
+    lista <- dplyr::group_split(lista, processo)
 
-    qpdf::pdf_combine(.x$arquivos, file.path(dir_destino,paste0(processo,".pdf")))
 
-  },NULL))
+    purrr::walk(lista, purrr::possibly(~{
+
+      processo <- unique(.x$processo)
+
+      qpdf::pdf_combine(.x$arquivos, file.path(dir_destino,paste0(processo,".pdf")))
+
+    },NULL))
+
+
+  } else {
+
+    lista <- dplyr::group_split(lista, processo, id_doc)
+
+    purrr::walk(lista, purrr::possibly(~{
+      processo <- unique(.x$processo)
+      id_doc <- unique(.x$id_doc)
+      suppressWarnings(
+        qpdf::pdf_combine(.x$arquivos, file.path(dir_destino,
+                                                 paste0(processo,"_id_doc_",id_doc, ".pdf")))
+      )
+    }, NULL))
+  }
+
+
 
 
 }
