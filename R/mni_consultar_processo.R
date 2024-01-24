7d0942e2fc73c8374184fbc491af38acb0b14644
José de Jesus Filho <jjesusfilho@gmail.com>
2024-01-24 13:55:37 -0300

    Update mni_consultar_processo.R

--- R/mni_consultar_processo.R
+++ R/mni_consultar_processo.R
@@ -24,11 +24,11 @@ mni_consultar_processo <- function(tribunal = "tjsp",
                                    incluir_documentos = FALSE,
                                    documentos = NULL,
                                    diretorio = "."){
-
+  
   usuario_key <- toupper(paste0(tribunal,"MNIUSUARIO"))
-
+  
   senha_key <- toupper(paste0(tribunal,"MNISENHA"))
-
+  
   if (is.null(usuario) || is.null(senha)) {
     usuario <- Sys.getenv(usuario_key)
     senha <- Sys.getenv(senha_key)
@@ -37,22 +37,20 @@ mni_consultar_processo <- function(tribunal = "tjsp",
       senha <- as.character(getPass::getPass(msg = "Entre com senha: "))
     }
   }
-
+  
   x <- c(movimentos = movimentos, cabecalho = cabecalho, incluir_documentos = incluir_documentos) |>
     ifelse("true","false")
-
+  
   processo <- stringr::str_remove_all(processo,"\\D")
-
+  
   url <-"http://esaj.tjsp.jus.br/mniws/servico-intercomunicacao-2.2.2/intercomunicacao?wsdl"
-
-  pb <- progress::progress_bar$new(total = length(processo))
+  
 
   purrr::walk(processo, purrr::possibly(~{
-
-    pb$tick()
+    
 
     ## A op\u00E7\u00E3o por documentos n\u00E3o convive com cabe\u00E7alho e movimentos.
-
+    
     corpo <- criar_corpo(
       cabecalho = x[["cabecalho"]],
       movimentos = x[["movimentos"]],
@@ -64,28 +62,33 @@ mni_consultar_processo <- function(tribunal = "tjsp",
     )
     if (incluir_documentos) {
       arquivo <- file.path(diretorio, paste0(tribunal,"_mni_incluir_documentos_", .x,".xml"))
-
+      
     } else if (movimentos){
-
+      
       arquivo <- file.path(diretorio, paste0(tribunal,"_mni_", .x,".xml"))
-
+      
     } else if (!is.null(documentos)){
-
+      
       arquivo <- file.path(diretorio, paste0(tribunal,"_mni_inteiro_teor_documentos_", .x,".xml"))
-
+      
+    } else if (cabecalho){
+      
+      arquivo <- file.path(diretorio, paste0(tribunal,"_mni_cabecalho_", .x,".xml"))
+      
+      
     }
-
+    
     httr::POST(url, body = corpo, httr::write_disk(arquivo, overwrite = T))
-
-  }, NULL))
-
+    
+  }, NULL), .progress = TRUE)
+  
 }
 
 
 criar_corpo <- function(cabecalho, movimentos, incluir_documentos,documentos, processo, usuario , senha){
-
+  
   if (incluir_documentos){
-
+    
     corpo <- glue::glue('<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://www.cnj.jus.br/servico-intercomunicacao-2.2.2/" xmlns:tip="http://www.cnj.jus.br/tipos-servico-intercomunicacao-2.2.2">
 <soapenv:Header/>
 <soapenv:Body>
@@ -93,13 +96,13 @@ criar_corpo <- function(cabecalho, movimentos, incluir_documentos,documentos, pr
 <tip:idConsultante>{usuario}</tip:idConsultante>
 <tip:senhaConsultante>{senha}</tip:senhaConsultante>
 <tip:numeroProcesso>{processo}</tip:numeroProcesso>
-<tip:incluirDocumentos>{documentos}</tip:incluirDocumentos>
+<tip:incluirDocumentos>{incluir_documentos}</tip:incluirDocumentos>
 </ser:consultarProcesso>
 </soapenv:Body>
 </soapenv:Envelope>')
-
+    
   } else if (movimentos){
-
+    
     corpo <- glue::glue('<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://www.cnj.jus.br/servico-intercomunicacao-2.2.2/" xmlns:tip="http://www.cnj.jus.br/tipos-servico-intercomunicacao-2.2.2">
 <soapenv:Header/>
 <soapenv:Body>
@@ -113,11 +116,11 @@ criar_corpo <- function(cabecalho, movimentos, incluir_documentos,documentos, pr
 </soapenv:Body>
 </soapenv:Envelope>')
   } else if (!is.null(documentos)){
-
-
+    
+    
     docs <- glue::glue("<tip:documento>{documentos}</tip:documento>") |>
       paste0(collapse = "\n")
-
+    
     corpo <- glue::glue('<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://www.cnj.jus.br/servico-intercomunicacao-2.2.2/" xmlns:tip="http://www.cnj.jus.br/tipos-servico-intercomunicacao-2.2.2">
 <soapenv:Header/>
 <soapenv:Body>
@@ -129,9 +132,26 @@ criar_corpo <- function(cabecalho, movimentos, incluir_documentos,documentos, pr
 </ser:consultarProcesso>
 </soapenv:Body>
 </soapenv:Envelope>')
-
-
+    
+  } else if(cabecalho){
+    
+    corpo <- glue::glue('<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://www.cnj.jus.br/servico-intercomunicacao-2.2.2/" xmlns:tip="http://www.cnj.jus.br/tipos-servico-intercomunicacao-2.2.2">
+<soapenv:Header/>
+<soapenv:Body>
+<ser:consultarProcesso>
+<tip:idConsultante>{usuario}</tip:idConsultante>
+<tip:senhaConsultante>{senha}</tip:senhaConsultante>
+<tip:numeroProcesso>{processo}</tip:numeroProcesso>
+<tip:incluirCabecalho>{cabecalho}</tip:incluirCabecalho>
+</ser:consultarProcesso>
+</soapenv:Body>
+</soapenv:Envelope>')
+    
+    
   }
   return(corpo)
+  
+}
+
 
 }
