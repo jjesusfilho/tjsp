678604732946082d3b6865f627ccd0ca4c713153
José de Jesus Filho <jjesusfilho@gmail.com>
2024-04-29 05:58:47 -0300

    Update tjsp_classificar_recurso.R

--- R/tjsp_classificar_recurso.R
+++ R/tjsp_classificar_recurso.R
@@ -12,12 +12,12 @@
 #'
 tjsp_classificar_recurso <- function (x)
 {
-
+  
   x<- x %>%
     stringr::str_sub(-2000) %>%
     tolower() %>%
     stringi::stri_trans_general("latin-ascii")
-
+  
   dplyr::case_when(
     stringi::stri_detect_regex(x, "(?=.*\\bderam\\b)(?=.*\\bneg[oa]\\w*\\b)") ~ "duvida",
     stringi::stri_detect_regex(x, "(?=.*\\bderam\\b)(?=.*\\bprejudicado\\b)") ~ "duvida",
@@ -29,11 +29,11 @@ tjsp_classificar_recurso <- function (x)
     stringi::stri_detect_regex(x, "nao\\s+conhec\\w+") ~ "desconhecido",
     stringi::stri_detect_regex(x, "(desprov\\w+|improv\\w+)") ~ "improvido",
     stringi::stri_detect_regex(x, "(nao|nega\\w+)\\s+provi\\X*") ~ "improvido",
-    stringi::stri_detect_regex(x, "provi\\w+") ~ "provido",
     stringi::stri_detect_regex(x, "mantiveram") ~ "improvido",
     stringi::stri_detect_regex(x, "acolh\\w+") ~ "provido",
     stringi::stri_detect_regex(x, "(deu|deram|da\\-*\\s*se|dando\\-*(se)*|comporta|\\bdou\\b|confere\\-se|se\\s*\\-*da|merece)") ~ "provido",
     stringi::stri_detect_regex(x, "(nao\\sderam|nao\\smerece|se\\snega|nega\\-*\\s*se|negar\\-*\\s*lhe|nao\\scomporta|negram|negararam|nego|negar)") ~ "improvido",
+    stringi::stri_detect_regex(x, "provi\\w+") ~ "provido",
     stringi::stri_detect_regex(x, "(homolog|desistencia)") ~ "desist\u00eancia",
     stringi::stri_detect_regex(x, "rejeit\\w+ (os)? embargos") ~ "embargos rejeitados",
     stringi::stri_detect_regex(x, "(anular\\w*|nulo|nula|nulidade)") ~ "anulado",
