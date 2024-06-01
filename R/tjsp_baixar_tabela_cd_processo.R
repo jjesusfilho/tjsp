ba40c96ba4c11ab52438b47c2cf4438d3d3e5cb1
José de Jesus Filho <jjesusfilho@gmail.com>
2024-06-01 07:17:13 -0300

    Adiciona possibilidade de baixar processos depre

--- R/tjsp_baixar_tabela_cd_processo.R
+++ R/tjsp_baixar_tabela_cd_processo.R
@@ -9,67 +9,63 @@
 tjsp_baixar_tabela_cd_processo <- function (cd_processo = NULL, diretorio = ".")
 {
   httr::set_config(httr::config(ssl_verifypeer = FALSE))
-
-
-  pb <- progress::progress_bar$new(total = length(cd_processo))
-
+  
   purrr::walk(cd_processo, purrr::possibly(~{
-
+    
     arquivo <- file.path(diretorio,paste0("tabela_cd_processo_",.x,".html"))
-
-
-     if (stringr::str_detect(.x, "^DW")){
+    
+    
+    url1 <- .x |>
+      paste0("https://esaj.tjsp.jus.br/cpopg/show.do?processo.codigo=", ... = _, "&gateway=true") 
+    
+    if (stringr::str_detect(.x, "^DW")){
       url1 <- paste0(url1,"&consultaDeRequisitorios=true")
     }
   
     r1 <- url1 |>
       httr::GET()
     
-
-
-
+    
     if (
-
-    r1 |>
+      
+      r1 |>
       httr::content() |>
       xml2::xml_find_first("boolean(//a[@class='linkConsultaSG btn btn-secondary btn-space'])")
-
+      
     ) {
-
-     cdProcesso <-  r1 |>
+      
+      cdProcesso <-  r1 |>
         httr::content() |>
         xml2::xml_find_first("//a[@class='linkConsultaSG btn btn-secondary btn-space']") |>
         xml2::xml_attr("href") |>
-       stringr::str_extract("(?<=Sg.)\\w+")
-
-    url2 <-  paste0("https://esaj.tjsp.jus.br/cposg/show.do?processo.codigo=",cdProcesso, "&gateway=true")
-
-
-     r2 <- httr::GET(url2)
-
-     url3  <- paste0("https://esaj.tjsp.jus.br/cposg/verificarAcessoPastaDigital.do?cdProcesso=",cdProcesso,"&conversationId=&_=1599440192646")
-
-
-
-     arquivo <- file.path(diretorio, paste0("tabela_cd_processo_pg_", .x, "_cd_processo_sg_",cdProcesso,".html"))
-
-
-     } else{
-
-    url3 <- paste0("https://esaj.tjsp.jus.br/cpopg/abrirPastaDigital.do?processo.codigo=",.x)
-
-    arquivo <- file.path(diretorio,paste0("tabela_cd_processo_pg_",.x,".html"))
-
-
-     }
-
-    url3|>
+        stringr::str_extract("(?<=Sg.)\\w+")
+      
+      url2 <-  paste0("https://esaj.tjsp.jus.br/cposg/show.do?processo.codigo=",cdProcesso, "&gateway=true")
+      
+      
+      r2 <- httr::GET(url2)
+      
+      url3  <- paste0("https://esaj.tjsp.jus.br/cposg/verificarAcessoPastaDigital.do?cdProcesso=",cdProcesso,"&conversationId=&_=1599440192646")
+      
+      
+      
+      arquivo <- file.path(diretorio, paste0("tabela_cd_processo_pg_", .x, "_cd_processo_sg_",cdProcesso,".html"))
+      
+      
+    } else{
+      
+      url3 <- paste0("https://esaj.tjsp.jus.br/cpopg/abrirPastaDigital.do?processo.codigo=",.x)
+      
+      arquivo <- file.path(diretorio,paste0("tabela_cd_processo_pg_",.x,".html"))
+      
+      
+    }
+    
+    url3 |>
       httr::GET() |>
       httr::content("text") |>
       httr::GET(httr::write_disk(arquivo,overwrite = TRUE))
-
-  }, NULL))
-
+    
+  }, NULL), .progress = TRUE)
+  
 }
-
-
