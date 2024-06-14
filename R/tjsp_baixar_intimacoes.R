82600a41e6874d3fd6a12715417b16992d7bf540
José de Jesus Filho <jjesusfilho@gmail.com>
2024-06-14 08:12:41 -0300

    Update tjsp_baixar_intimacoes.R

--- R/tjsp_baixar_intimacoes.R
+++ R/tjsp_baixar_intimacoes.R
@@ -1,3 +1,4 @@
+
 #' Coleta intimações do TJSP
 #'
 #' @param cd_foro Código do Foro
@@ -26,58 +27,58 @@ tjsp_baixar_intimacoes <- function(cd_foro = "",
                                    versao = c("xml","csv"),
                                    intervalo = "trimestral",
                                    diretorio = "."){
-
-
+  
+  
   cd_foro <- as.integer(cd_foro)
-
+  
   ato_fl_cumprido <- switch(situacao,
                             ambas = "",
                             cumprida = "S",
                             pendente = "N"
   )
-
+  
   if(dt_inicio == ""){
-
+    
     dt_inicio <- (Sys.Date() - 1) |> format("%d/%m/%Y")
   }
-
+  
   if(dt_fim == ""){
-
+    
     dt_fim <- (Sys.Date() -1) |> format("%d/%m/%Y")
   }
-
+  
   cd_tipo_ato <- switch(natureza_comunicacao,
                         ambas = "-1",
                         intimacao = "0",
                         citacao = "4"
   )
-
+  
   versao <- versao[[1]]
-
+  
   url1 <- switch(versao,
-
+                 
                  csv =  "https://esaj.tjsp.jus.br/intimacoesweb/exportarAtosRecebidosParaCsv.do",
-
+                 
                  xml= "https://esaj.tjsp.jus.br/intimacoesweb/consultarAtosRecebidos.do"
-
+                 
   )
-
+  
   e <- switch(versao,
               csv = ".csv",
               xml = ".xml"
   )
-
+  
   cd_usuario <- Sys.getenv("ESAJ_CD_USUARIO")
-
-
+  
+  
   datas <- agrupar_datas(dt_inicio, dt_fim, corte = intervalo)
-
-
+  
+  
   purrr::walk2(datas$data_inicial, datas$data_final, ~{
-
- di <- .x
- df <- .y
-
+    
+    di <- .x
+    df <- .y
+    
     corpo <-
       list(
         conversationId = "",
@@ -109,111 +110,121 @@ tjsp_baixar_intimacoes <- function(cd_foro = "",
         entity.cdTipoAto = cd_tipo_ato,
         entity.ato.flCumprido = ato_fl_cumprido
       )
-
+    
     r1 <-  httr::POST(url1, body = corpo, encode = "form") |>
-          httr::content()
-
-
-   dividir <- `/`
-
-   paginas <- r1 |>
-              xml2::xml_find_first("//*[@id='textQtLinhasRow']/following-sibling::text()") |>
-              xml2::xml_text() |>
-              stringr::str_remove_all("\\D+") |>
-              as.integer() |>
-              dividir(20) |>
-              ceiling()
-
-   i <- lubridate::dmy(di) |>
-     stringr::str_replace_all("\\D","_")
-
-   f <- lubridate::dmy(df) |>
-     stringr::str_replace_all("\\D","_")
-
-   purrr::walk(0:paginas, ~{
-
-     p  <- .x
-
-   arquivo <- file.path(diretorio,paste0('pagina_',p,"_foro_", cd_foro,"_inicio_",i,"_fim_",f,e))
-
-   q <- structure(
-     list(
-       scheme = "https",
-       hostname = "esaj.tjsp.jus.br",
-       port = NULL,
-       path = "intimacoesweb/AjaxServlet.ajax",
-       query = list(
-         component = "gridPaginada",
-         filterPaginationID = "/intimacoesweb_gridAtosUsuario",
-         beanClass = "br.com.softplan.intimacoes.comum.AtoUsuario",
-         objectPaginationHandler = "br.com.softplan.saj.intimacoesweb.grid.ConsultaAtosRecebidosPaginationHandler",
-         ejbClass = "",
-         ejbMethod = "",
-         pageSize = "20",
-         page = .x,
-         currentPage = "0",
-         paramSize = "0",
-         orderBy = "null",
-         orderByDirection = "null",
-         column0Type = "null",
-         column0 = "cdAto",
-         column1Type = "null",
-         column1 = "cdUsuario",
-         column2Type = "null",
-         column2 = "ato.cdProcesso",
-         column3Type = "null",
-         column3 = "ato.cdForo",
-         column4Type = "null",
-         column4 = "ato.especialidade.cdEspecialidade",
-         column5Type = "null",
-         column5 = "ato.cargo.cdCargo",
-         column6Type = "null",
-         column6 = "ato.flCumprido",
-         column7Type = "null",
-         column7 = "ato.cdTipoAto",
-         column8Type = "null",
-         column8 = "intimacaoAutomatica",
-         column9Type = "null",
-         column9 = "dtLeitura",
-         column10Type = "null",
-         column10 = "ato.dtInclusao",
-         column11Type = "null",
-         column11 = "dtIntimacao",
-         column12Type = "null",
-         column12 = "nuDiasPrazo",
-         column13Type = "null",
-         column13 = "ato.nuProcessoFormat",
-         column14Type = "null",
-         column14 = "classeEAssuntoPrincipaisFormatado",
-         column15Type = "null",
-         column15 = "usuarioIntimacao.nmUsuario",
-         column16Type = "null",
-         column16 = "ato.deTipoMvProcesso",
-         column17Type = "null",
-         column17 = "ato.especialidade.nmEspecialidade",
-         column18Type = "null",
-         column18 = "ato.cargo.deCargo",
-         column19Type = "null",
-         column19 = "ato.tarjasJson",
-         columnsSize = "20",
-         objSerializado = "null"
-       ),
-       params = NULL,
-       fragment = NULL,
-       username = NULL,
-       password = NULL
-     ),
-     class = "url"
-   )
-
-
-   q$query$page <- p
-
-   parseada <- httr::build_url(q)
-
-   httr::GET(parseada, httr::write_disk(arquivo, overwrite = T))
-
-})
-
+      httr::content()
+    
+    
+    dividir <- `/`
+    
+    paginas <- r1 |>
+      xml2::xml_find_first("//*[@id='textQtLinhasRow']/following-sibling::text()") |>
+      xml2::xml_text() |>
+      stringr::str_remove_all("\\D+") |>
+      as.integer() |>
+      dividir(20) |>
+      ceiling()
+    
+    i <- lubridate::dmy(di) |>
+      stringr::str_replace_all("\\D","_")
+    
+    f <- lubridate::dmy(df) |>
+      stringr::str_replace_all("\\D","_")
+    
+    
+    p <- paginas:1
+    
+    purrr::walk(p, ~{
+      
+   
+      if (.x == max(p)){
+        
+        page <- 2
+        current_page <- 0
+      } else {
+        
+        page <- 3
+        current_page <- .x
+      }
+      
+      arquivo <- file.path(diretorio,paste0('pagina_',.x,"_foro_", cd_foro,"_inicio_",i,"_fim_",f,e))
+      
+      q <- structure(
+        list(
+          scheme = "https",
+          hostname = "esaj.tjsp.jus.br",
+          port = NULL,
+          path = "intimacoesweb/AjaxServlet.ajax",
+          query = list(
+            component = "gridPaginada",
+            filterPaginationID = "/intimacoesweb_gridAtosUsuario",
+            beanClass = "br.com.softplan.intimacoes.comum.AtoUsuario",
+            objectPaginationHandler = "br.com.softplan.saj.intimacoesweb.grid.ConsultaAtosRecebidosPaginationHandler",
+            ejbClass = "",
+            ejbMethod = "",
+            pageSize = "20",
+            page = page,
+            currentPage = current_page,
+            paramSize = "0",
+            orderBy = "null",
+            orderByDirection = "null",
+            column0Type = "null",
+            column0 = "cdAto",
+            column1Type = "null",
+            column1 = "cdUsuario",
+            column2Type = "null",
+            column2 = "ato.cdProcesso",
+            column3Type = "null",
+            column3 = "ato.cdForo",
+            column4Type = "null",
+            column4 = "ato.especialidade.cdEspecialidade",
+            column5Type = "null",
+            column5 = "ato.cargo.cdCargo",
+            column6Type = "null",
+            column6 = "ato.flCumprido",
+            column7Type = "null",
+            column7 = "ato.cdTipoAto",
+            column8Type = "null",
+            column8 = "intimacaoAutomatica",
+            column9Type = "null",
+            column9 = "dtLeitura",
+            column10Type = "null",
+            column10 = "ato.dtInclusao",
+            column11Type = "null",
+            column11 = "dtIntimacao",
+            column12Type = "null",
+            column12 = "nuDiasPrazo",
+            column13Type = "null",
+            column13 = "ato.nuProcessoFormat",
+            column14Type = "null",
+            column14 = "classeEAssuntoPrincipaisFormatado",
+            column15Type = "null",
+            column15 = "usuarioIntimacao.nmUsuario",
+            column16Type = "null",
+            column16 = "ato.deTipoMvProcesso",
+            column17Type = "null",
+            column17 = "ato.especialidade.nmEspecialidade",
+            column18Type = "null",
+            column18 = "ato.cargo.deCargo",
+            column19Type = "null",
+            column19 = "ato.tarjasJson",
+            columnsSize = "20",
+            objSerializado = "null"
+          ),
+          params = NULL,
+          fragment = NULL,
+          username = NULL,
+          password = NULL
+        ),
+        class = "url"
+      )
+      
+  
+      parseada <- httr::build_url(q)
+      
+      httr::GET(parseada, httr::write_disk(arquivo, overwrite = T))
+      
+    })
+    
   })
 }
