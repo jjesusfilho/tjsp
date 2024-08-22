e303934a734935ea4a843571a1ab565bf6a24e67
José de Jesus Filho <jjesusfilho@gmail.com>
2024-08-22 08:20:20 -0300

    Fix: Corrige função tjsp_intimacoes_foros

--- R/tjsp_intimacoes_foros.R
+++ R/tjsp_intimacoes_foros.R
@@ -8,7 +8,7 @@ tjsp_intimacoes_foros <- function(){
 
   url1 <- "https://esaj.tjsp.jus.br/intimacoesweb/AjaxServlet.ajax?tagId=foroService&cdUsuarioReferencia=251219&cdTipoAto=-1&cdInstancia=1&filtroValue=&component=inputSelect&bind_nmForo=entity.nmForo&bind_cdForo=entity.cdForo&useAction=false&SpwInputSelectRequestOrigin=InputSelectSearchGrid&currentTime=1724319940295"
 
-  r1 <- httr::GET(url2)
+  r1 <- httr::GET(url1)
 
   url2 <- "https://esaj.tjsp.jus.br/intimacoesweb/jsp/search/searchForo.jsp?nmForo=&reference=entity.nmForo&reference=entity.nmForo&entity.nmForo=&entity.nmForo=&=&=&=&idObjRetorno=foroService&multiselection=false&cdUsuarioReferencia=251219&cdTipoAto=-1&cdInstancia=1&height=282&inputSelectId=foroService&CurrentMultSelecaoId=foroService&multSelecaoProperty=null&gotInputParam=false&bind_nmForo=entity.nmForo&bind_cdForo=entity.cdForo&useAction=false&requesterUrl=jsp/search/searchForo.jsp?nmForo=&SpwInputSelectRequestOrigin=InputSelectSearchGrid&desabilitarSelecionados=false"
 
