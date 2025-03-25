50cc00d1a537d941fa557c1577486aecb468766e
José de Jesus Filho <jjesusfilho@gmail.com>
2025-03-25 06:44:18 -0300

    Feat: Adiciona autenticação em duas etapas

--- R/ler_execucao_penal.R
+++ R/ler_execucao_penal.R
@@ -7,25 +7,24 @@
 #' @export
 #'
 tjsp_baixar_execucao_penal_cd_processo <- function(cdprocesso, diretorio = "."){
-  
+
   cdprocesso <- stringr::str_extract(cdprocesso,"\\w+")
-  
-  
+
+
   purrr::walk(cdprocesso, purrr::possibly(~{
-    
-    
+
+
     arquivo <- file.path(diretorio, paste0("execucao_penal_cd_processo_",.x, ".html"))
-    
-    httr::GET(paste0("https://esaj.tjsp.jus.br/cpopg/show.do?processo.codigo=",.x,"&gateway=true")) |> 
-      httr::content() |> 
-      xml2::xml_find_first("//a[contains(@href,'nuSeqParte')]") |> 
-      xml2::xml_attr('href') |> 
-      xml2::url_absolute("https://esaj.tjsp.jus.br") |> 
+
+    httr::GET(paste0("https://esaj.tjsp.jus.br/cpopg/show.do?processo.codigo=",.x,"&gateway=true")) |>
+      httr::content() |>
+      xml2::xml_find_first("//a[contains(@href,'nuSeqParte')]") |>
+      xml2::xml_attr('href') |>
+      xml2::url_absolute("https://esaj.tjsp.jus.br") |>
       httr::GET(httr::write_disk(arquivo, overwrite = T))
-    
+
   }, NULL), .progress = TRUE)
 }
-
 
 #' Lê os metadados da execução penal
 #'
@@ -35,77 +34,78 @@ tjsp_baixar_execucao_penal_cd_processo <- function(cdprocesso, diretorio = "."){
 #' @returns tibble
 #' @export
 #'
+#'
 tjsp_ler_execucao_penal_cd_processo <- function(arquivos  = NULL, diretorio = "."){
-  
+
   if(is.null(arquivos)){
-    
+
     arquivos <- list.files(diretorio, full.names = TRUE, pattern = "cd_processo")
-    
+
   }
-  
+
   purrr::map(arquivos, purrr::possibly(~{
-    
-    
+
+
     tjsp_ler_execucao_penal_cd_processo1(.x)
-    
+
   },NULL))
 }
 
-#' L\u00EA apenas um arquivo da execu\u00E7\u00E3o
+#' Lêapenas um arquivo da execução
 #'
 #' @param arquivo Caminho para arquivo
 #'
 #' @returns tibble
 #'
 tjsp_ler_execucao_penal_cd_processo1 <- function(arquivo){
-  
-  
+
+
   x <- xml2::read_html(arquivo)
-  
+
   cdprocesso <- arquivo |>
     stringr::str_extract("(?<=cd_processo_)\\w+")
-  
-  
-  nome <- x |> 
-    xml2::xml_find_all("//h2[@class='subtitle tituloDoBloco']") |> 
-    xml2::xml_text() |> 
+
+
+  nome <- x |>
+    xml2::xml_find_all("//h2[@class='subtitle tituloDoBloco']") |>
+    xml2::xml_text() |>
     purrr::pluck(2)
-  
-  label <- x |> 
-    xml2::xml_find_all("//td[@class='label']|//td/div[@class='label']") |> 
-    xml2::xml_text() |> 
+
+  label <- x |>
+    xml2::xml_find_all("//td[@class='label']|//td/div[@class='label']") |>
+    xml2::xml_text() |>
     snakecase::to_snake_case(transliterations = "Latin-ASCII")
-  
-  valores <- x |> 
-    xml2::xml_find_all("//td[@class='label']/following-sibling::td|//td/div[@class='label']/following-sibling::text()") |> 
+
+  valores <- x |>
+    xml2::xml_find_all("//td[@class='label']/following-sibling::td|//td/div[@class='label']/following-sibling::text()") |>
     xml2::xml_text(trim = T)
-  
-  principal <- tibble::tibble(variavel = label, valor = valores) |> 
-    tibble::add_row(variavel = "nome", valor= nome, .before = 1) |> 
-    tibble::add_column(cdprocesso, .before = 1) 
-  
-  documentos <- x |> 
-    xml2::xml_find_first("//div[h2[text()='Documentos']]/following-sibling::table") |> 
-    rvest::html_table(convert = F) |> 
-    stats::setNames(c("documento_sigla","documento","documento_expedidor")) |> 
-    dplyr::filter(!dplyr::if_all(dplyr::everything(),.fns = ~.x == "")) |> 
+
+  principal <- tibble::tibble(variavel = label, valor = valores) |>
+    tibble::add_row(variavel = "nome", valor= nome, .before = 1) |>
     tibble::add_column(cdprocesso, .before = 1)
-  
-  historico <- x |> 
-    xml2::xml_find_first("//div[h2[text()='Hist\u00F3rico da parte']]/following-sibling::table") |> 
-    rvest::html_table(convert = F) |> 
-    stats::setNames(c("data_evento","evento","complemento")) |> 
-    dplyr::filter(!dplyr::if_all(dplyr::everything(),.fns = ~.x == "")) |> 
-    dplyr::mutate(data_evento = lubridate::dmy(data_evento))  |> 
+
+  documentos <- x |>
+    xml2::xml_find_first("//div[h2[text()='Documentos']]/following-sibling::table") |>
+    rvest::html_table(convert = F) |>
+    stats::setNames(c("documento_sigla","documento","documento_expedidor")) |>
+    dplyr::filter(!dplyr::if_all(dplyr::everything(),.fns = ~.x == "")) |>
     tibble::add_column(cdprocesso, .before = 1)
-  
-  link <- x |> 
-    xml2::xml_find_first("//script/text()[contains(.,'PG5')]") |> 
-    xml2::xml_text() |> 
-    stringr::str_extract("cdProcesso.+(?=')") |> 
-    xml2::url_absolute("https://esaj.tjsp.jus.br/cpopg/consultarDadosParte.do?") |> 
+
+  historico <- x |>
+    xml2::xml_find_first("//div[h2[text()='Hist\u00F3rico da parte']]/following-sibling::table") |>
+    rvest::html_table(convert = F) |>
+    stats::setNames(c("data_evento","evento","complemento")) |>
+    dplyr::filter(!dplyr::if_all(dplyr::everything(),.fns = ~.x == "")) |>
+    dplyr::mutate(data_evento = lubridate::dmy(data_evento))  |>
+    tibble::add_column(cdprocesso, .before = 1)
+
+  link <- x |>
+    xml2::xml_find_first("//script/text()[contains(.,'PG5')]") |>
+    xml2::xml_text() |>
+    stringr::str_extract("cdProcesso.+(?=')") |>
+    xml2::url_absolute("https://esaj.tjsp.jus.br/cpopg/consultarDadosParte.do?") |>
     tibble::tibble(cdprocesso, link = _)
-  
+
   list(principal, documentos, historico, link)
-  
+
 }
--- R/tjsp_autenticar.R
+++ R/tjsp_autenticar.R
@@ -2,6 +2,8 @@
 #'
 #' @param login cpf
 #' @param password senha
+#' @param email_provider Informar se "gmail" ou "outlook"
+#' @param outlook Informar se "personal" ou "business"
 #' @return Estabelece uma sessão, não é necessário salvar.
 #' @export
 #'
@@ -9,7 +11,18 @@
 #'      criar variáveis de ambiente: "LOGINADV" e "PASSWORDADV", ou
 #'      chamar a função e aguardar o prompt para informar
 #'      login e password
-tjsp_autenticar <- function(login = NULL, password = NULL) {
+tjsp_autenticar <- function(login = NULL,
+                            password = NULL,
+                            email_provider = NA,
+                            outlook = "business"
+                            ) {
+
+
+  email_provider <- match.arg(email_provider, choices = c("gmail","outlook"))
+
+  outlook <- match.arg(outlook, choices = c("personal","business"))
+
+
 
   # Check if isn't already logged in
   if (check_login()) {
@@ -34,7 +47,10 @@ tjsp_autenticar <- function(login = NULL, password = NULL) {
   base <- "https://esaj.tjsp.jus.br/"
   httr::GET(stringr::str_c(base, "esaj/portal.do?servico=740000"), httr::config(ssl_verifypeer = FALSE))
 
-  # Get login page file
+  hora <- lubridate::now(tzone = "America/Sao_Paulo") |>
+    as.POSIXct() |>
+    as.numeric()
+
   f_login <- stringr::str_c(
     base, "sajcas/login?service=",
     utils::URLencode(
@@ -43,6 +59,7 @@ tjsp_autenticar <- function(login = NULL, password = NULL) {
     )
   ) |>
     httr::GET(httr::config(ssl_verifypeer = FALSE))
+
 
   # Get parameters for POST
   lt <- f_login |>
@@ -57,6 +74,7 @@ tjsp_autenticar <- function(login = NULL, password = NULL) {
     xml2::xml_find_first("//input[@name='execution']") |>
     xml2::xml_attr("value")
 
+
   # Create POST quert
   query_post <- list(
     username = login,
@@ -70,7 +88,9 @@ tjsp_autenticar <- function(login = NULL, password = NULL) {
     certificado = ""
   )
 
-  # Try to login
+
+
+  # Primeira etapa
  stringr::str_c(
     base, "sajcas/login?service=",
     utils::URLencode(
@@ -79,6 +99,36 @@ tjsp_autenticar <- function(login = NULL, password = NULL) {
     )
   ) |>
     httr::POST(body = query_post, httr::config(ssl_verifypeer = FALSE), encode = "form")
+
+
+
+ taxa <- purrr::rate_backoff(pause_base = 10, max_times = 5)
+
+ get_email_token_insistente <- purrr::insistently(get_email_token1, taxa, quiet = FALSE)
+
+ get_token <- purrr::possibly(get_email_token_insistente, otherwise = "N\u00E3o foi poss\u00EDvel gerar o token")
+
+ token <- get_token(email_provider,outlook,hora)
+
+ if(stringr::str_detect(token,"\\d", negate = T)){
+
+   stop(token)
+ }
+
+  query_post_token <- list(username = login, password = password,
+                           lt = lt, execution = e2, token = token, `_eventId` = "submit",
+                           signature = "", certificadoSelecionado = "", certificado = "")
+
+
+  stringr::str_c(
+    base, "sajcas/login?service=",
+    utils::URLencode(
+      stringr::str_c(base, "esaj/j_spring_cas_security_check"),
+      reserved = TRUE
+    )
+  ) |>
+    httr::POST(body = query_post_token, httr::config(ssl_verifypeer = FALSE), encode = "form")
+
 
   # Message
   flag <- check_login()
@@ -103,3 +153,54 @@ check_login <- function() {
 #' @rdname tjsp_autenticar
 #' @export
 autenticar <- tjsp_autenticar
+
+
+#' Obtêm tokens do email
+#'
+#' @param email_provider "gmail" ou "outlook"
+#' @param outlook Se outlook, informar se "business" ou
+#'    "personal"
+#' @param hora Parâmetro não usado, por ora.
+#'
+#' @returns token
+#'
+get_email_token1 <- function(email_provider, outlook, hora) {
+
+  if(email_provider == "outlook"){
+
+    if(outlook=="business"){
+
+    outl <- Microsoft365R::get_business_outlook()
+
+    } else{
+      outl <- Microsoft365R::get_personal_outlook()
+    }
+
+    email <- outl$list_emails(
+      search = "from:esaj@tjsp.jus.br AND subject:Valida\u00E7\u00E3o",
+      n = 1
+    )
+
+    token <- email[[1]]$properties$bodyPreview |>
+      stringr::str_extract("\\d{6}")
+
+  } else {
+
+
+  #gmailr::gm_auth()
+
+  filtro <- glue::glue("from:esaj@tjsp.jus.br AND subject:Valida\u00E7\u00E3o")
+
+  token <- gmailr::gm_threads(search= filtro) |>
+  purrr::pluck(1,"threads",1,"snippet") |>
+  stringr::str_extract("\\d+")
+
+}
+
+  stopifnot(stringr::str_detect(token,"\\d+"))
+
+  return(token)
+
+}
+
+
